18bb2f95f376b4dff525cc293a5f732c6455eb17
evgenii2104 <evgenii2104@rambler.ru>
2023-01-13 02:06:43 +0300

    x

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const l = 0
+console.log(document.getElementById('timer'));
+
+const timerVal = document.getElementById('timer');
+
+
+function initTimer() {
+   let seconds = 30;
+   const timer = setInterval(() => {
+     if(seconds < 1) {
+       timerVal.style.display = 'none';
+       let massag = prompt('Вы победили в конкурсе', '');
+       alert(massag);
+       clearInterval(timer);
+     }
+     timerVal.textContent = `До окончания курса осталось: 00:00:${seconds} секунд`;
+     seconds -= 1;
+   }, 1000);
+}
+
+initTimer();
