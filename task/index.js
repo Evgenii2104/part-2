e58c3776463d2e40e81a2655e7d17d504bf3c27f
evgenii2104 <evgenii2104@rambler.ru>
2023-01-23 23:12:26 +0300

    q

--- task/index.js
+++ task/index.js
@@ -3,13 +3,13 @@ console.log(document.getElementById('timer'));
 const timerVal = document.getElementById('timer');
 
 
-function initTimer() {
-   let seconds = 30;
+/*function initTimer() {
+   let seconds = 5;
    const timer = setInterval(() => {
      if(seconds < 0) {
        timerVal.style.display = 'none';
-       let massag = prompt('Вы победили в конкурсе', '');
-       alert(massag);
+       
+       alert('Вы победили в конкурсе');
        clearInterval(timer);
      }
      timerVal.textContent = `До окончания курса осталось: 00:00:${seconds} секунд`;
@@ -17,4 +17,41 @@ function initTimer() {
    }, 1000);
 }
 
-initTimer();
+initTimer();*/
+
+
+let seconds = 900;
+
+function chekTime() {
+  const timer = setInterval(() => {
+    
+    if(seconds < 0) {
+      timerVal.style.display = 'none';
+      alert('Вы победили в конкурсе');
+      clearInterval(timer);
+    }
+  let hour = 0;
+  let min = 0;
+  let sec = seconds;
+  
+   if ( sec > 60 && sec <= 3600) {
+      min = Math.floor(seconds / 60);
+      sec = seconds - (60 * min)
+    } if (sec > 3600) {
+      sec = Math.floor(seconds - (60 * 60)) 
+      min = Math.floor(seconds / 60);
+      hour = Math.floor(min / 60);
+    }
+  hour = hour < 10 ? '0' + hour : hour;
+  min = min < 10 ? '0' + min : min;
+  sec = sec < 10 ? '0' + sec : sec;
+
+     timerVal.textContent = `До окончания курса осталось: ${hour + ':' + min + ':' + sec} секунд`;
+     console.log(hour + ':' + min + ':' + sec)
+     seconds -= 1
+}, 1000)  
+}
+ 
+ chekTime()
+
+
