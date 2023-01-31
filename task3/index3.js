23970c7932ce816ab495e4974168d33f52efc227
evgenii2104 <evgenii2104@rambler.ru>
2023-02-01 01:22:13 +0300

    begin

---
+++ task3/index3.js
@@ -0,0 +1,23 @@
+let sliderPrev = document.getElementsByClassName('slider__arrow_prev')[0]
+let sliderNext = document.getElementsByClassName('slider__arrow_next')[0]
+let sliders = Array.from(document.querySelectorAll('.slider__item'))
+let actSliders = Array.from(document.getElementsByClassName('slider__item slider__item_active'))
+
+console.log(sliderPrev, sliderNext)
+console.log(sliders)
+console.log(actSliders)
+
+sliderPrev.onclick = () => {
+    sliders.forEach((slider) => {
+        for (let i = 0; i < sliders.length; i++)
+        console.log(sliders.length)
+        actSliders.forEach((actSlider) => {
+            actSlider.classList.remove('slider__item_active')
+            slider[i + 1]
+            slider.classList.add('slider__item_active')
+
+        })
+        
+
+    })
+}
