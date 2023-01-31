c543e31cc97cf35ab369fa9446d59e5fc7b46980
evgenii2104 <evgenii2104@rambler.ru>
2023-01-31 23:53:07 +0300

    n

--- index.js
+++ index.js
@@ -1,18 +1,104 @@
-//let but = Array.from(document.querySelector('li'))
-let button = document.getElementsByClassName('menu menu_main')[0]
-let butt = button.querySelector('.menu__item')
+
+/*const menuItems = Array.from(document.querySelectorAll('.menu_main > .menu__item'))
+
+
+menuItems.forEach((menuItem) => {
+    let link = menuItem.querySelector('a')
+    let menuSub = menuItem.querySelector('.menu_sub')
+    if (menuSub !== undefined) {
+        link.onclick = (event) => {
+            menuSub.classList.toggle('menu_active')
+            event.preventDefault()
+        }
+    }
+})*/
+
+
+const menuItems = Array.from(document.querySelectorAll('.menu_main > .menu__item'))
+
+
+menuItems.forEach((menuItem) => {
+    let link = menuItem.querySelector('a')
+    link.onclick = (event) => {
+        let menuSub = menuItem.querySelector('.menu_sub')
+        if (menuSub !== undefined) {
+            let menuActs = Array.from(document.querySelectorAll('.menu_sub.menu_active'))
+            console.log(menuActs)
+            if (menuActs.length > 0) {
+                menuActs.forEach((menuAct) => {
+                menuAct.classList.remove('menu_active')
+            })
+        } else {
+            menuSub.classList.add('menu_active')
+            event.preventDefault()
+        }
+        //menuSub.classList.add('menu_active')
+        //event.preventDefault()        
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+/*let but = document.querySelectorAll('ul .menu_sub')
+let butt = document.querySelectorAll('.menu__item + .menu_sub')
+//let button = document.getElementsByClassName('menu menu_main')[0]
+//let butt = button.querySelector('.menu__item')
 //let button1 = button.querySelector('.menu menu_sub')
 let links = Array.from(document.querySelectorAll('a'))
 //let links = link.querySelector('.menu__link')
-console.log(button)
 console.log(butt)
-
-links.forEach((link) => {
+console.log(but)
+*/
+/*links.forEach((link) => {
     link.onclick = (event) => {
         console.log(link)
         event.preventDefault()
     }
 })
+*/
+
+/*butt.forEach((but) => {
+    but.onclick = (but.target) => {
+        console.log(but.target)
+        //but.classList.add('menu_active')
+        //but.nextSibling.add('menu_active')
+        //butt.children.add('menu_active')
+    }
+})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.querySelector('.menu').addEventListener("click", function(event) {
+                let target = event.target;
+                if (target = target.closest(".menu_active")) {
+                    let parent = target.parentNode;
+                    let children = parent.parentNode.children;
+                    [...children].forEach(el => el.classList[ el === parent ? "toggle" : "remove"]("menu_sub"))
+                }
+            });
+    */
+
+
 
 
    /*button.onmouseover = () => {
