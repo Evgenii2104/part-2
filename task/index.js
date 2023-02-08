f94139979a0bd2f60ba431091081a88da011aa53
evgenii2104 <evgenii2104@rambler.ru>
2023-02-08 23:17:22 +0300

    z

--- task/index.js
+++ task/index.js
@@ -1,26 +1,25 @@
 let but = document.querySelector('.dropdown__value')
-let men = document.querySelector('.dropdown__list')
+let menus = document.querySelector('.dropdown__list')
 
-console.log(but, men)
+
+console.log(but, menus)
 
 but.onclick = () => {
-    //let targget = event.targget
-   // let targget = this.value
     console.log(but.textContent)
-    //but.textContent = targget.parentNode 
-    men.classList.toggle('dropdown__list_active')
-    men.onclick = (event) => {
-        //let targget = event.currentTargget
-        console.log(this.targget, targget)
-        but.textContent = men.event.textContent
+    menus.classList.toggle('dropdown__list_active')
 
-    }
-
+    let texs = Array.from(document.querySelectorAll('a'))
+    console.log(texs)
+    texs.forEach((tex) => {
+        tex.onclick = (event) => {
+        
+        let targget = event.target
+        console.log(targget)
+        but.textContent = targget.textContent
+        }
+    })
 }
 
-/*let c = document.querySelectorAll('.dropdown__list')
-c.addEventListener('change', function(){  
-    var getValue = this.value;
-    // this в этом контексте - элемент, который запустил фукнцию. То же, что и select.value;
-    console.log( getValue );
-  });*/
+
+
+
