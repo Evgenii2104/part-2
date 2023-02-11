c295e2bc26bbeaa5e7bddc23edaf4580c3093a3f
evgenii2104 <evgenii2104@rambler.ru>
2023-02-11 16:08:34 +0300

    c

--- task2/index2.js
+++ task2/index2.js
@@ -4,24 +4,40 @@ let loss = document.querySelector('.status__loss')
 console.log(win, loss)
 
 let strs = Array.from(document.querySelectorAll('.word > .symbol'))
-
+//let strTexts = strs.textContent
 console.log(strs)
-
+let count = 0
+let countWin = 0
+    let countLoss = 0
 function curretTru (event) {
+    
+    
     //console.log(event)
     //console.log(event instanceof KeyboardEvent)
     console.log(event.key) //
-    //console.log(event.code)
-    strs.forEach((str) => {
-        console.log(str)
-        if (event.key === str) {
-            str.classList.add('symbol_correct')
-            win.innerHTML += 1
+
+    if (count < strs.length) {
+            let str = strs[count]
+            console.log(str, count)
+        
+        if (event.key === str.textContent) {
+           str.classList.add('symbol_correct'),
+            countWin++,
+            count++
+            
         } else {
-            str.classList.add('word_incorrect')
-            loss.innerHTML += 1
+            str.classList.add('word_incorrect'),
+            countLoss++,
+            count++
         }
-    })
+        
+    } else {
+        alert(`Правильно введённых слов:${countWin}
+        Неправильно введённых слов:${countLoss} `)
+    }
+    
+    win.innerHTML = `${countWin}`, 
+    loss.innerHTML = `${countLoss}`
 }
 
 document.addEventListener('keydown', curretTru)
