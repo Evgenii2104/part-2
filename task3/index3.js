d0ad7dfbdfe974ebf23a58aac0e0f1fc719d1060
evgenii2104 <evgenii2104@rambler.ru>
2023-02-15 23:48:43 +0300

    vg

--- task3/index3.js
+++ task3/index3.js
@@ -1,12 +1,35 @@
-let size = Array.from(document.querySelectorAll('.font-size'))
+let size = Array.from(document.querySelectorAll('a.font-size'))
+let content = document.querySelector('.book__content')
 //let target = null
 console.log(size)
+//let act = document.querySelector('.font-size_active')
 
-size.onclick = (event) => {
-    let target = event
-    console.log(target.classNamet)
-    /*size.forEach((siz) => {
-        //let target = siz.getAttribute('font-size') 
-        //console.log(target.className)
-    })*/
+
+
+/*size.onclick = () => {
+for(let i = 0; i < size.length; i++) {   
+    
+        //let act = document.querySelector('.font-size_active')
+         //   act.classList.remove('font-size_active')
+    
+    size[i].classList.toggle('font-size_active')
+    console.log(size.length)
 }
+}*/
+
+
+    
+    size.forEach((siz) => {
+        siz.onclick = () => {
+            //let act = document.querySelector('.font-size_active')
+            //act.classList.remove('font-size_active')
+            //console.log(act)
+            
+            siz.classList.add('font-size_active')
+            content.classList.add()
+        }
+    })
+
+        
+        
+   
