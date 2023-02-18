bb7ba0aa7c5b0facca30d767b0c1a749158ec187
evgenii2104 <evgenii2104@rambler.ru>
2023-02-18 18:32:47 +0300

    bh

--- task3/index3.js
+++ task3/index3.js
@@ -1,23 +1,65 @@
 let sliderPrev = document.getElementsByClassName('slider__arrow_prev')[0]
 let sliderNext = document.getElementsByClassName('slider__arrow_next')[0]
 let sliders = Array.from(document.querySelectorAll('.slider__item'))
-let actSliders = Array.from(document.getElementsByClassName('slider__item slider__item_active'))
+//let actSliders = Array.from(document.getElementsByClassName('slider__item_active'))
 
 console.log(sliderPrev, sliderNext)
-console.log(sliders)
-console.log(actSliders)
+console.log(sliders.length)
 
-sliderPrev.onclick = () => {
-    sliders.forEach((slider) => {
-        for (let i = 0; i < sliders.length; i++)
-        console.log(sliders.length)
-        actSliders.forEach((actSlider) => {
-            actSlider.classList.remove('slider__item_active')
-            actSlider += slider[i + 1]
-            actSlider.classList.add('slider__item_active')
+let count = 1
+//document.addEventListener('click', showNext)
+document.addEventListener('click', showPrev)
 
-        })
-        
 
-    })
-}
+
+ function showNext() {
+    
+    sliderNext.onclick = () => {
+         let actSliders = document.querySelector('.slider__item_active')
+         actSliders.classList.remove('slider__item_active')
+         sliders[count].classList.add('slider__item_active')           
+         console.log(sliders[count])
+        }
+        count++
+        if (count >= sliders.length) {
+            count = 0
+        }
+
+
+        }
+    
+    
+    function showPrev() {
+        sliderPrev.onclick = () => {
+            if (count <= sliders.length) {
+                count = 6
+            }
+            let actSliders = document.querySelector('.slider__item_active')
+            actSliders.classList.remove('slider__item_active')
+            sliders[count].classList.add('slider__item_active')           
+            console.log(sliders[count])
+           }
+           count--
+           console.log(count)
+           
+    }    
+    
+ 
+console.log(count)
+
+
+
+
+
+/*sliders.forEach((slider, index) => {
+sliderNext.onclick = () => {
+    
+        let actSliders = document.querySelector('.slider__item_active')
+       actSliders.classList.remove('slider__item_active')
+       slider.classList.add('slider__item_active')
+       count++
+       console.log(index)
+} 
+
+})
+*/
